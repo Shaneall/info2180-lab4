a02be3d8564581621e0cb0d8dbcd2e8ee3dfea18
Shan <shaneal.forrest@mymona.uwi.edu>
2016-10-15 00:00:46 +0000

    final

--- maze.js
+++ maze.js
@@ -1,13 +1,66 @@
-//Exercise 1
-function change(){
-	document.getElementById('boundary1').className += 'youlose' ;
-}
-
 window.onload = function(){
-	document.getElementById('boundary1').onmouseenter = change;
-}
-
-function bound(){
+	document.getElementById("start").onclick = function(){
+	 var count = 0;
+	 var maze = document.getElementById('maze');
+	 function change(){
+	 	document.getElementById('boundary1').className += ' youlose' ;
+	 	count+=1;
+	 	document.getElementById("status").innerHTML = "YOU LOSE!!!";
+	
+		}
 	var boundaries = document.querySelectorAll('div.boundary');
+	for(var s=0; s < boundaries.length-1; s++)
+	{
+		boundaries[s].onmouseover = function()
+		{
+			count +=1;
+			for(var s=0; s < boundaries.length-1; s++)
+			{
+				boundaries[s].className += ' youlose' ;
+				document.getElementById("status").innerHTML = "YOU LOSE!!!";
+		}
+	}
+	
 
+	document.getElementById("end").onmouseover = function(){
+		if ( count > 0){
+		
+			document.getElementById("status").innerHTML = "YOU LOSE!!!";
+		}
+		else
+		{
+			document.getElementById("status").innerHTML = "YOU WIN!!!";
+		}
+	
+	}
+
+		count=0;
+		var boundaries = document.querySelectorAll('div.boundary');
+			count +=1;
+			for(var s=0; s < boundaries.length-1; s++)
+			{
+				boundaries[s].className = 'boundary';
+			}
+		document.onmouseover = function(num){
+			var left = maze.offsetLeft;
+			var	right = maze.offsetRight;
+			var top = maze.offsetTop;
+			var bottom = maze.offset;
+			var height = maze.offsetHeight;
+			var width = maze.offsetWidth;
+			var s = num.clientX;
+			var f = num.clientY;
+
+			if ((s < left) || (s > right))
+			{
+				for(var s=0; s < boundaries.length-1; s++)
+					{
+						boundaries[s].className += ' youlose' ;
+						count += 1;
+						document.getElementById("status").innerHTML = "YOU LOSE!!!";
+				}
+			}
+		}
+	}
+}
 }
